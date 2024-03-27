342ea7a2b7c62d38f2294028f10008d77c564379
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-03-27 17:27:06 +0300

    класс BoundaryService, тесты BoundaryServiceTest, файл build.yml.

---
+++ src/test/java/ananasovitch/org/githubactions/BoundaryServiceTest.java
@@ -0,0 +1,42 @@
+package ananasovitch.org.githubactions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoundaryServiceTest {
+
+    @Test
+    @DisplayName("Проверка метода findMin с положительными числами")
+    public void testFindMinWithPositiveNumbers() {
+        BoundaryService boundaryService = new BoundaryService();
+        int[] marks = {7, 3, 9, 5, 1};
+        assertEquals(1, boundaryService.findMin(marks));
+    }
+
+    @Test
+    @DisplayName("Проверка метода findMin с отрицательными числами")
+    public void testFindMinWithNegativeNumbers() {
+        BoundaryService boundaryService = new BoundaryService();
+        int[] marks = {-7, -3, -9, 0, -5, -1};
+        assertEquals(-9, boundaryService.findMin(marks));
+    }
+
+    @Test
+    @DisplayName("Проверка метода findMin с одним положительным числом")
+    public void testFindMinWithSinglePositiveNumber() {
+        BoundaryService boundaryService = new BoundaryService();
+        int[] marks = {10};
+        assertEquals(10, boundaryService.findMin(marks));
+    }
+
+    @Test
+    @DisplayName("Проверка метода findMin с одним отрицательным числом")
+    public void testFindMinWithSingleNegativeNumber() {
+        BoundaryService boundaryService = new BoundaryService();
+        int[] marks = {-10};
+        assertEquals(-10, boundaryService.findMin(marks));
+    }
+}
+
